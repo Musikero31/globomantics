6be7e51c4fcfd1619a5a7bdb8e28c4597d2dff51
Ryan <ryan.aimel.estandarte@gmail.com>
2026-04-29 18:10:13 -0500

    User inputs and forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
-//import { useCallback, useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
-//import navValues from "./navigation/navValues";
 import { BrowserRouter, Route, Routes } from "react-router";
 import HouseList from "./components/HouseList";
 import House from "./components/House";
 
 function App() {
-  // const navigate = useCallback(
-  //   (navTo, param) => setNav({current: navTo, param, navigate}),
-  //   []
-  // )
-  // const [nav, setNav] = useState({current: navValues.home, navigate})
-
   return (
-    /*
-    <navigationContext.Provider value={nav}>
-      <ErrorBoundary fallback="Something went wrong!">
-        <Banner>
-          <div>Providing houses all over the world!!!!</div>
-        </Banner>
-        <ComponentPicker currentNavLocation={nav.current}/>
-      </ErrorBoundary>
-    </navigationContext.Provider>
-    */
     <BrowserRouter>
       <Banner>
         <div>Providing houses all over the world!!!!</div>
       </Banner>
       <Routes>
         <Route index element={<HouseList/>}/>
-        <Route path="house/:id" element={<House/>} />
+        <Route path="house" element={<House/>} />
       </Routes>
     </BrowserRouter>
   );
